7087b7427221f30411d5120a702c4273197dca96
Artur19t <artur19t@yandex.ru>
2025-11-15 14:20:15 +0300

    add new sensors of temp and humidity. Refinement of the i2c data exchange algorithm

--- SmartHome/Users/Inc/i2creadwrite.h
+++ SmartHome/Users/Inc/i2creadwrite.h
@@ -4,13 +4,21 @@
 #include <stdbool.h>
 #include "main.h"
 
-#define BH1750_ADDR         (0x23 << 1)  // ????? (<<1 ??? 8-??????? ???????)
-#define BH1750_CMD_POWER_ON 0x01
-#define BH1750_CMD_RESET    0x07
-#define BH1750_CMD_CONT_H   0x10
+#define BH1750_ADDR          (0x23 << 1)
+#define BH1750_CMD_POWER_ON  0x01
+#define BH1750_CMD_RESET     0x07
+#define BH1750_CMD_CONT_H    0x10
 
+#define SHT21_ADDR           (0x40 << 1)
+#define SHT21_NO_HOLD_TEMP   0xF3
+#define SHT21_NO_HOLD_HUMID  0xF5
+
+uint16_t Read2Bytes(uint8_t address);
+void ReadNBytes(uint8_t address, uint8_t *buf, uint16_t N);
 float BH1750_ReadLux(void);
-void BH1750_WriteCmd(uint8_t cmd);
+float SHT21_ReadTemperature(void);
+float SHT21_ReadHumidity(void);
 void BH1750_Init(void);
-static int I2C_WriteCmd(uint8_t cmd);
+static int I2C_WriteCmd(uint8_t cmd, uint8_t address);
+
 #endif
